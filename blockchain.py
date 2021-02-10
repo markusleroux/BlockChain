e6961b02bdddfddefcdf00cdc9fb0478047ece02
Markus Le Roux <markusleroux314@gmail.com>
2021-02-10 23:06:46 +0800

    Implement details of UTXO_set, introduce full node

--- blockchain.py
+++ blockchain.py
@@ -9,7 +9,14 @@ import hashlib
 class Address:
     def __init__(self):
         self.private_key = ecdsa.SigningKey.generate()
-        self.UTXOs = []  # (txid, value, output index)
+        self.UTXOs = dict()     # txid : MyUTXO(txid, output_index, value)
+
+    class MyUTXO:
+        def __init__(self, txid, output_index, value):
+            self.txid = txid 
+            self.output_index = output_index
+            self.value = value
+            self.pending = False
 
     def public_key(self):
         return self.private_key.verifying_key
@@ -17,49 +24,80 @@ class Address:
     def sign(self, byte_string):
         return self.private_key.sign(byte_string)
 
-    def create_locking_script(self, receiver_pubkey):
+    def p2pk_locking_script(self, receiver_pubkey):
         return receiver_pubkey
 
-    def create_unlocking_script(self, UTXO):
-        return self.sign(self.public_key) 
+    def p2pk_unlocking_script(self, UTXO):
+        return self.sign(self.public_key)
+
+    def reset_pending_transactions(self):
+        for UTXO in self.UTXOs.values():
+            UTXO.pending = False
+
+    def clear_pending_transactions(self):
+        for UTXO in self.UTXOs.values():
+            if UTXO.pending:
+                del UTXOs[UTXO.txid]
 
     def gather_UTXOs(self, value):
-        i, remaining_value = 0, value
-        while remaining_value > 0:
-            if i >= len(self.UTXOs):
-                raise ValueError("Insufficient funds") 
+        remaining_value = value
+        gathered_UTXO = set()
+        for UTXO in self.UTXOs.values():
+            if remaining_value <= 0:
+                break
 
-            remaining_value -= self.UTXOs[i][1]
-            i += 1
+            remaining_value -= UTXO.value
+            gathered_UTXO.add(UTXO)
 
-        return (i, abs(remaining_value))
+        if remaining_value > 0:
+            raise ValueError("Insufficient funds")
+            
+        return (gathered_UTXO, abs(remaining_value))
 
-    def create_transaction(self, value, reciever_pubkey):
+    def generate_lock_unlock(self, tx_type = "p2pk"):
+        if tx_type == "p2pk":
+            locking_script = self.p2pk_locking_script
+            unlocking_script = self.p2pk_unlocking_script
+
+        return (unlocking_script, locking_script)
+
+    def create_transaction(self, value, reciever_pubkey, tx_type = "p2pk"):
+        unlock, lock = self.generate_lock_unlock(tx_type) 
         inputs, outputs = [], []
 
-        (i, change) = self.gather_UTXOs(value)
-        for UTXO in UTXOs[:i]: 
-            unlocking_script = self.create_unlocking_script()
+        (gathered_UTXO, extra_value) = self.gather_UTXOs(value)
+        for UTXO in gathered_UTXO:
+            UTXO.pending = True
+            unlocking_script = unlock()
             inputs.append(TxInput(UTXO, unlocking_script))
-            
-        locking_script, locking_script_for_change = self.create_locking_script(receiver_pubkey), self.create_locking_script(self.public_key)
+
+        locking_script = lock(receiver_pubkey)
+        locking_script_for_change = lock(self.public_key)
+
         outputs.append(TxOutput(value, locking_script))
         outputs.append(TxOutput(change, locking_script_for_change))
 
-        return (Transaction(inputs, outputs), i)
+        return Transaction(inputs, outputs)
+
+    def create_genesis(self, receiver_pubkey, tx_type = "p2pk"):
+        _, lock = self.generate_lock_unlock(tx_type)        
+        locking_script = lock(receiver_pubkey)
+        return Transaction([], [TxOutput(50, locking_script)])
+
 
 def iterable_hash(self, hashable_it):
-    '''Helper function which constructs a hash from an iterable list of hashable items'''
+    """Helper function which constructs a hash from an iterable list of hashable items"""
     m = hashlib.sha256()
     for item in hashable_it:
         m.update(item)
 
     return m.digest()
-    
+
+
 class TxInput:
     def __init__(self, UTXO, unlocking_script):
-        self.prev_txid = UTXO[0] 
-        self.output_index = UTXO[2]
+        self.prev_txid = UTXO.txid
+        self.output_index = UTXO.output_index
         self.unlocking_script = unlocking_script
 
     def __hash__(self):
@@ -74,9 +112,10 @@ class TxOutput:
     def __hash__(self):
         return iterable_hash((self.value.to_bytes(), self.locking_script))
 
+
 class Transaction:
     def __init__(self, inputs, outputs):
-        self.inputs = inputs 
+        self.inputs = inputs
         self.outputs = outputs
 
     def __hash__(self):
@@ -85,9 +124,9 @@ class Transaction:
 
 class BlockHeader:
     def __init__(self, prev_block_hash, merkle_root_hash):
-        self.merkle_root_hash = merkle_root_hash 
+        self.merkle_root_hash = merkle_root_hash
         self.nonce = 0
-        self.prev_block_hash = prev_block_hash 
+        self.prev_block_hash = prev_block_hash
         self.timestamp = time.time()
 
     def increment_nonce(self):
@@ -99,10 +138,17 @@ class BlockHeader:
 
     @staticmethod
     def has_proof_of_work(self):
-        return int.from_bytes(hash(self)) < 10**7
+        return int.from_bytes(hash(self)) < 10 ** 7
 
     def __hash__(self):
-        return iterable_hash((self.merkle_root_hash, self.prev_block_hash, self.nonce._to_bytes(), self.timestamp.to_bytes()))
+        return iterable_hash(
+            (
+                self.merkle_root_hash,
+                self.prev_block_hash,
+                self.nonce._to_bytes(),
+                self.timestamp.to_bytes(),
+            )
+        )
 
 
 class Block:
@@ -113,11 +159,13 @@ class Block:
         self.header.proof_of_work()
 
     def contains_txid(self, txid):
-        return self.transactions_merkle.verify_leaf_inclusion(txid, self.transactions_merkle.get_proof(txid))
+        return self.transactions_merkle.verify_leaf_inclusion(
+            txid, self.transactions_merkle.get_proof(txid)
+        )
 
     def dump_tx(self, txid):
         pass
-    
+
     def __hash__(self):
         # The header contains the merkle root hash, which ensures that
         # this defines a unique hash for each block
@@ -131,61 +179,98 @@ class BlockChain:
     def add_block(self, block):
         self.chain.append(block)
 
+    def fits_blockchain_head(self, new_block):
+        return (
+            new_block.header.has_proof_of_work()
+            and new_block.header.prev_block_hash == hash(self.head())
+        )
 
-class Miner:
-    def __init__(self, blockchain = BlockChain()):
+    def head(self):
+        return self.chain[-1]
+
+
+class InvalidBlock(Exception):
+    def __init__(self, block):
+        self.block = block
+        self.block_hash = hash(block)
+        self.message = "Invalid block"
+
+    def __str__(self):
+        return f"{self.block_hash}: {self.message}"
+
+
+class FullNode:
+    def __init__(self, blockchain):
         self.blockchain = blockchain
-        self.block_hash_to_index = dict()
-        self.UTXO_set = dict()
+        self.build_UTXO_set()
 
-    def get_block_by_hash(block_hash):
-        return self.blockchain.chain[self.block_hash_to_index[block_hash]]
+    def build_UTXO_set(self):
+        self.UTXO_set = self.init_UTXO_set()
+        for block in self.blockchain.chain[1:]:
+            self.update_UTXO_set(block)
+
+    def update_UTXO_set(self, block):
+        for tx in block.transactions:
+            try:
+                for tx_in in tx.inputs:
+                    del UTXO_set[hash(tx_in)]
+
+                for tx_out in tx.outputs:
+                    UTXO_set[hash(tx_out)] = tx_out
+
+            except KeyError:
+                raise InvalidBlock(block)
+
+    def init_UTXO_set():
+        pass
 
     def validate_block(self, block):
-        if not block.header.prev_block_hash == hash(self.blockchain.chain[-1]) or block.header.has_proof_of_work():
+        if not self.blockchain.fits_blockchain_head(block):
             return False
 
         for tx in block.transactions:
-            if not validate_tx(tx):
+            if not self.validate_tx(tx):
                 return False
 
         return True
 
-    def add_block(self, block):
-        if not validate_block(block):
-            raise ValueError("Invalid block")
-        
-        self.blockchain.add_block(block)
-        self.block_hash_to_index[hash(block)] = len(self.chain)
-        self.update_UTXO_set(block)
-
-    def update_UTXO_set(self, block):
-        for tx in block.transactions:
-            for tx_in in tx.inputs:
-                del UTXO_set[hash(tx_in)]
-
-            for tx_out in tx.outputs:
-                UTXO_set[hash(tx_out)] = (tx_out, hash(block))
-        
     def validate_tx(self, tx):
+        # Assumes the transaction is happening at the end of the current blockchain
+
         cash = 0
         # Verify all inputs have unlocking script for corresponding output locking script
         # Collect sum of UTXO value
-        for tx_in in tx.inputs:
-            try:
-                # Check that the tx is in the block
-                self.get_block_by_hash(self.UTXO_set[hash(tx)][1]).contains_txid(hash(tx))
-                
-                old_tx_out = self.UTXO_set[tx_in.prev_txid][0].outputs[tx_in.output_index]
-                if not p2pk_verify(old_tx_out.locking_script, tx_in.unlocking_script):
-                    return False
-                
-            except KeyError:
+        for txi in tx.inputs:
+            # Check that the tx is in the UTXO_set
+            if not txi.prev_txid in UTXO_set:
                 return False
 
-        # Verify the amount in output is less than cash on hand
-        for tx_out in tx.outputs:
-            cash -= tx_out.value
+            # Verify that the unlocking script unlocks the locking script
+            prev_txo = UTXO_set[txi.prev_txid].outputs[txi.output_index]
+            if not p2pk_verify(prev_txo.locking_script, txi.unlocking_script):
+                return False
+
+            # Collect the value of the previous output as cash on hand
+            cash += prev_txo.value
+
+        # Verify the amount output by this transaction is less than cash on hand
+        for txo in tx.outputs:
+            cash -= txo.value
 
         return cash >= 0
-            
+
+
+class Miner:
+    def __init__(self, blockchain):
+        self.blockchain = blockchain
+
+    def add_block(self, block):
+        # Assumes the transactions on the block are valid
+        # but checks that the headers are in order
+        if not self.blockchain.fits_blockchain_head(block):
+            raise InvalidBlock(block)
+
+        self.blockchain.add_block(block)
+
+    def create_block(self, transactions):
+        return Block(self.blockchain.head(), transactions)
