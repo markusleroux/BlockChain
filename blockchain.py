49b51a4c5ecf364cfc6fe049eb9c4045c5b8ec27
Markus Le Roux <markusleroux314@gmail.com>
2021-02-13 23:31:44 +0800

    Add most type hints

--- blockchain.py
+++ blockchain.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
+from typing import Iterable
 import time
 import math
+from dataclasses import dataclass
 import bitarray
 import mmh3
 import merklelib
@@ -9,49 +11,94 @@ import ecdsa
 import hashlib
 
 
-class Address:
-    def __init__(self):
-        self.private_key = ecdsa.signingKey.generate()
-        self._myUTXOs = dict()  # txid : MyUTXO(txid, output_index, value)
+def sha256_hash(item: bytes) -> int:
+    """Helper function which constructs a hash from an iterable list of hashable items"""
+    m = hashlib.sha256()
+    m.update(item)
+    return int.from_bytes(m.digest(), "big")
 
+
+# could be dataclass
+class TxOutput:
+    def __init__(self, value: float, locking_script: str):
+        self.value = value
+        self.locking_script = locking_script
+
+    def __key(self):
+        return (self.value.to_bytes(4, 'big'), self.locking_script)
+
+    def __hash__(self):
+        return sha256_hash(self.__key())
+
+
+class TxInput:
+    def __init__(self, UTXO: TxOutput, unlocking_script):
+        self.prev_txid: int = UTXO.txid
+        self.output_index: int = UTXO.output_index
+        self.unlocking_script = unlocking_script
+
+    def __key(self):
+        return (self.prev_txid, self.output_index, self.unlocking_script)
+
+    def __hash__(self):
+        return sha256_hash(self.__key())
+
+
+class Transaction:
+    def __init__(self, inputs: Iterable[TxInput], outputs: Iterable[TxOutput]):
+        self.inputs = list(inputs)
+        self.outputs = list(outputs)
+
+    def __key(self):
+        return tuple(io for io in self.inputs + self.outputs)
+
+    def __hash__(self):
+        return sha256_hash(self.__key())
+
+
+class Address:
+    @dataclass
     class MyUTXO:
-        def __init__(self, txid, output_index, value):
-            self.txid = txid
-            self.output_index = output_index
-            self.value = value
-            self.pending = False
+        txid: bytes
+        output_index: int
+        value: float
+        pending: bool = False
+
+   def __init__(self):
+        self.private_key: ecdsa.SigningKey = ecdsa.signingKey.generate()
+        self._myUTXOs: dict[int, MyUTXO] = dict()  # txid : MyUTXO(txid, output_index, value)
 
     @property
-    def public_key(self):
+    def public_key(self) -> str:
         return self.private_key.verifying_key.to_string()
 
-    def _sign(self, byte_string):
+    def _sign(self, byte_string: bytes):
         return self.private_key.sign(byte_string)
 
-    def generate_unlock_lock(self, tx_type="p2pk"):
+    def generate_unlock_lock(self, tx_type: str = "p2pk"):
         '''Return pair of unlocking and locking functions.'''
         if tx_type == "p2pk":
             return (self._p2pk_unlocking_script, self.__p2pk_locking_script)
 
-    def _p2pk_locking_script(self, receiver_pubkey):
+    def _p2pk_locking_script(self, receiver_pubkey: str):
         return receiver_pubkey
 
     def _p2pk_unlocking_script(self):
         return self._sign(self.public_key)
 
-    def _incoming_tx(self, txid, output_index, value):
+    def _incoming_tx(self, txid: int, output_index: int, value: float):
         '''Add a transaction to myUTXO set'''
         self[txid] = self.MyUTXO(txid, output_index, value)
 
-    def _reset_pending_transaction(self, txid):
+    def _reset_pending_transaction(self, txid: int):
         '''Reset UTXO when transaction has failed.'''
         self[txid].pending = False
 
-    def _clear_pending_tx(self, txid):
+    def _clear_pending_tx(self, txid: int):
         '''Remove UTXO when transaction has cleared.'''
         del self[txid]
 
-    def _gather_UTXOs(self, value):
+    def _gather_UTXOs(self, value: float) -> tuple(set[MyUTXO], float):
         '''Get a set of UTXO with combined value greater than value.'''
         remaining_value = value
         gathered_UTXO = set()
@@ -67,7 +114,7 @@ class Address:
 
         return (gathered_UTXO, abs(remaining_value))
 
-    def create_transaction(self, value, receiver_pubkey, tx_type="p2pk"):
+    def create_transaction(self, value: float, receiver_pubkey: str, tx_type: str = "p2pk") -> Transaction:
         '''Return a transaction sending value amount to receiver_pubkey address.'''
         unlock, lock = self.generate_unlock_lock(tx_type)
         inputs, outputs = [], []
@@ -86,7 +133,7 @@ class Address:
 
         return Transaction(inputs, outputs)
 
-    def create_genesis_tx(self, receiver_pubkey, tx_type="p2pk"):
+    def create_genesis_tx(self, receiver_pubkey: str, tx_type: str = "p2pk"):
         '''Return a transaction sending 50 coin to receiver pubkey without using UTXO.'''
         _, lock = self.generate_unlock_lock(tx_type)
         locking_script = lock(receiver_pubkey)
@@ -110,25 +157,25 @@ class BloomFilter:
 
     https://github.com/bitcoin/bips/blob/master/bip-0037.mediawiki#filter-matching-algorithm
     '''
-    def __init__(self, N, P):
+    def __init__(self, N: int, P: float):
         self.N = N
         self.P = P
         self.size = self.get_size(N, P)
         self.n_hashes = self.get_n_hashes(N, self.size)
         self.bitarray = bitarray.bitarray(self.size)
 
-    def add(self, item):
+    def add(self, tx: Transaction):
         '''Add an item (usually, tx) to the filter.'''
         for i in range(self.n_hashes):
             # Index error?
-            self.bitarray[mmh3.hash(item, i)] = True
+            self.bitarray[mmh3.hash(tx, i)] = True
 
-    def check(self, item):
+    def check(self, tx: Transaction) -> bool:
         '''Check if an item is in the filter.'''
-        return any(self.bitarray(mmh3.hash(item, i)) for i in range(self.n_hashes))
+        return all(self.bitarray(mmh3.hash(tx, i)) for i in range(self.n_hashes))
 
     @staticmethod
-    def get_size(N, P):
+    def get_size(N: int, P: float) -> int:
         '''Return the size of the filter necessary to ensure false positive rate P.
 
         Arguments
@@ -141,7 +188,7 @@ class BloomFilter:
         return int(((-1 / (math.log(2) ** 2)) * N * math.log(P)) / 8)
 
     @staticmethod
-    def get_n_hashes(N, S):
+    def get_n_hashes(N: int, S: int) -> int:
         '''The number of hash functions required for a filter of size S with N elements.
 
         Arguments
@@ -157,52 +204,8 @@ class SimplifiedPaymentVerification:
     def __init__(self, headers):
         self.headers = headers
 
-    def verify_tx(self, txid):
+    def verify_tx(self, txid: int):
         NotImplemented
-
-
-def sha256_hash(item):
-    """Helper function which constructs a hash from an iterable list of hashable items"""
-    m = hashlib.sha256()
-    m.update(item)
-    return int.from_bytes(m.digest(), "big")
-
-
-class TxInput:
-    def __init__(self, UTXO, unlocking_script):
-        self.prev_txid = UTXO.txid
-        self.output_index = UTXO.output_index
-        self.unlocking_script = unlocking_script
-
-    def __key(self):
-        return (self.prev_txid, self.output_index, self.unlocking_script)
-
-    def __hash__(self):
-        return sha256_hash(self.__key())
-
-
-class TxOutput:
-    def __init__(self, value, locking_script):
-        self.value = value
-        self.locking_script = locking_script
-
-    def __key(self):
-        return (self.value.to_bytes(4, 'big'), self.locking_script)
-
-    def __hash__(self):
-        return sha256_hash(self.__key())
-
-
-class Transaction:
-    def __init__(self, inputs, outputs):
-        self.inputs = inputs
-        self.outputs = outputs
-
-    def __key(self):
-        return tuple(io for io in self.inputs + self.outputs)
-
-    def __hash__(self):
-        return sha256_hash(self.__key())
 
 
 class BlockHeader:
@@ -221,7 +224,7 @@ class BlockHeader:
             self.increment_nonce()
 
     @staticmethod
-    def has_proof_of_work():
+    def has_proof_of_work() -> bool:
         '''Predicate to determine if a block has proof of work.'''
         # return int.from_bytes(hash(self)) < 10 ** 7
         return True
@@ -241,24 +244,25 @@ class BlockHeader:
 
 
 class Block:
-    def __init__(self, transactions, prev_block_hash=None):
-        self._transactions = transactions
+    def __init__(self, transactions: Iterable[Transaction], prev_block_hash=None):
+        # better to store as dictionary
+        self._transactions = list(transactions)
         self.transactions_merkle = merklelib.MerkleTree(transactions)
         self.header = BlockHeader(self.transactions_merkle.merkle_root.encode(), prev_block_hash)
         self.header.proof_of_work()
 
-    def contains_txid(self, txid):
+    def contains_txid(self, txid: int) -> bool:
         '''Verify tx specified by txid is contained in block.'''
         return self.transactions_merkle.verify_leaf_inclusion(
             txid, self.transactions_merkle.get_proof(txid))
 
-    def _dump_tx(self, txid):
+    def _dump_tx(self, txid: int):
         pass
 
-    def __getitem__(self, tx_number):
+    def __getitem__(self, tx_number: int) -> Transaction:
         return self._transactions[tx_number]
 
-    def __setitem__(self, tx_number):
+    def __setitem__(self, tx_number: int):
         NotImplemented
 
     def __key(self):
@@ -274,10 +278,10 @@ class Block:
 
 
 class BlockChain:
-    def __init__(self, genesis_block):
-        self._chain = [genesis_block]
+    def __init__(self, genesis_block: Block):
+        self._chain: list[Block] = [genesis_block]
 
-    def add_block(self, block):
+    def add_block(self, block: Block):
         '''Add a block to the blockchain.
 
         Conditions
@@ -291,7 +295,7 @@ class BlockChain:
 
         raise InvalidBlock(block)
 
-    def __getitem__(self, height):
+    def __getitem__(self, height: int) -> Block:
         return self._chain[height]
 
     def __setitem__(self, height, block):
@@ -302,7 +306,7 @@ class BlockChain:
 
 
 class InvalidBlock(Exception):
-    def __init__(self, block):
+    def __init__(self, block: Block):
         self.block = block
         self.block_hash = hash(block)
         self.message = "Invalid block"
@@ -334,16 +338,16 @@ class UTXOSet:
     value
         block height and value
     '''
-    def __init__(self, blockchain):
+    def __init__(self, blockchain: BlockChain):
         self.blockchain = blockchain
-        self._data = dict()
+        self._data: dict[tuple[int, dict[int, TxOutput]]] = dict()
         for tx in blockchain[0]:
             self[hash(tx)] = (0, {index: txo for index, txo in enumerate(tx.outputs)})
 
         for block in self.blockchain[1:]:
             self.update(block)
 
-    def update(self, block):
+    def update(self, block: Block):
         '''Update the UTXO set with the transactions in the block.
 
         Notes: assumes the blockchain has not been updated.
@@ -359,7 +363,7 @@ class UTXOSet:
 
             self[hash(tx)] = (len(self.blockchain), {index: txo for index, txo in enumerate(tx.outputs)})
 
-    def __getitem__(self, txid):
+    def __getitem__(self, txid: int) -> tuple[int, dict[int, TxOutput]]:
         return self._data[txid]
 
     def __setitem__(self, txid, value):
@@ -367,11 +371,11 @@ class UTXOSet:
 
 
 class FullNode:
-    def __init__(self, blockchain):
+    def __init__(self, blockchain: BlockChain):
         self.blockchain = blockchain
         self.UTXO_set = UTXOSet(blockchain)
 
-    def validate_block(self, block):
+    def validate_block(self, block: Block) -> bool:
         '''Validate a new block.
 
         Conditions:
@@ -394,7 +398,7 @@ class FullNode:
 
         return True
 
-    def validate_tx(self, tx):
+    def validate_tx(self, tx: Transaction) -> bool:
         '''Validate a new transaction.
 
         Conditions:
@@ -421,23 +425,28 @@ class FullNode:
         return cash >= 0
 
     def verify_lock_unlock(self,
-                           locking_script,
+                           locking_script: bytes,
                            unlocking_script,
-                           tx_type="p2pk"):
+                           tx_type: str = "p2pk") -> bool:
         '''Verify that the unlocking script unlocks the locking script.'''
         if tx_type == "p2pk":
-            return ecdsa.signingKey.from_string(locking_script).verify(unlocking_script, locking_script)
+            return ecdsa.VerifyingKey.from_string(locking_script).verify(unlocking_script, locking_script)
         return True
 
-    def filter_block(self, block, bloom_bitarray):
-        NotImplemented
+    def filter_block(self, block: Block, bloom_bitarray: BloomFilter) -> set[int]:
+        matches = set()
+        for tx in block:
+            bloom_bitarray.check(tx)
+            matches.add(hash(tx))
 
-    def generate_proofs(self, block, txids):
+        return matches
+
+    def generate_proofs(self, block: Block, txids: Iterable[int]):
         return [block.transactions_merkle.get_proof(txid) for txid in txids]
 
 
 class Miner:
-    def __init__(self, blockchain=None):
+    def __init__(self, blockchain: BlockChain = None):
         self.address = Address()
         if not blockchain:
             # Create genesis_block
@@ -447,46 +456,46 @@ class Miner:
         else:
             self.blockchain = blockchain
 
-    def add_block(self, block):
+    def add_block(self, block: Block):
         '''Add a block to the local copy of the blockchain.'''
         self.blockchain.add_block(block)
 
-    def _create_block(self, transactions):
+    def _create_block(self, transactions: Iterable[Transaction]) -> Block:
         '''Create a new block from a list of transactions.'''
         return Block(transactions, hash(self.blockchain[-1]))
 
 
 class NetworkControl:
-    def __init__(self, n_miners, n_addresses, n_full_nodes):
-        self.addresses = {hash(ad): ad for ad in (Address() for _ in range(n_addresses))}
+    def __init__(self, n_miners: int, n_addresses: int, n_full_nodes: int):
+        self.addresses: dict[int, Address] = {hash(ad): ad for ad in (Address() for _ in range(n_addresses))}
 
-        self.miners = set()
+        self.miners: set[Miner] = set()
         genesis_miner = self.add_miner()
         for _ in range(n_miners):
             _ = self.add_miner(genesis_miner.blockchain)
 
-        self.full_nodes = set()
+        self.full_nodes: set[FullNode] = set()
         for _ in range(n_full_nodes):
             self.add_full_node(genesis_miner.blockchain)
 
         self.tx_queue = []
 
-    def add_miner(self, blockchain=None):
+    def add_miner(self, blockchain: BlockChain = None) -> Miner:
         '''Add a new miner to the set of miners and add their address to the list of addresses.'''
         miner = Miner(blockchain)
         self.addresses[hash(miner.address)] = miner.address
         self.miners.add(miner)
         return miner
 
-    def add_full_node(self, blockchain):
+    def add_full_node(self, blockchain: BlockChain):
         '''Add a new full node to the list of full nodes.'''
         self.full_nodes.add(FullNode(blockchain))
 
-    def enqueue_tx(self, tx):
+    def enqueue_tx(self, tx: Transaction):
         '''Add a new transaction to the queue of transactions.'''
         self.tx_queue.append(tx)
 
-    def dequeue_txs(self, n):
+    def dequeue_txs(self, n: int):
         '''Remove the first n transactions from the list of transactions.'''
         self.tx_queue = self.tx_queue[n:]
 
